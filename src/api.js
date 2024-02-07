a05f42033c1ebe0f02d37675c976f0f78b092c95
Jason Hoomalu <jason.hoomalu@gmail.com>
2024-02-07 03:02:13 -0700

    home function server call

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require("cors")
 const app = express();
 const router = express.Router();
+app.use(cors())
 
 router.get("/", (req, res) => {
   res.status(200).json({
